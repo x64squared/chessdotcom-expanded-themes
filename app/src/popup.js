6dd7d8d5806447f7ba3397657840165ca9137c5a
x64squared <108487529+x64squared@users.noreply.github.com>
2023-09-04 18:04:18 -0700

    fix default value selection on install

--- app/src/popup.js
+++ app/src/popup.js
@@ -5,35 +5,48 @@ const DROPDOWN_MENU_PARAMS = [
         elementId: 'board-theme-select',
         storageKey: 'BoardTheme',
         msgCmd: 'setBoardTheme',
+        defaultValue: 'default',
     },
     {
         elementId: 'piece-theme-select',
         storageKey: 'PieceTheme',
         msgCmd: 'setPieceTheme',
+        defaultValue: 'default',
     },
     {
         elementId: 'highlight-color-use-custom',
         storageKey: 'HighlightColorUseCustom',
         msgCmd: 'highlightColor',
+        defaultValue: false,
     },
     {
         elementId: 'highlight-color-value',
         storageKey: 'HighlightColorValue',
         msgCmd: 'highlightColor',
+        defaultValue: '#9cc700',
     },
 ];
 
-function dropDownMenuActions(elementId, storageKey, msgCmd) {
+function dropDownMenuActions(elementId, storageKey, msgCmd, defaultValue) {
     const selectMenu = document.getElementById(elementId);
 
+    // load saved values on open
     chrome.storage.sync.get(storageKey, (result) => {
-        if (selectMenu.type === 'checkbox') {
-            selectMenu.checked = result[storageKey];
+        if (Object.keys(result).length > 0) {
+            if (selectMenu.type === 'checkbox') {
+                selectMenu.checked = result[storageKey];
+            } else {
+                selectMenu.value = result[storageKey];
+            }
         } else {
-            selectMenu.value = result[storageKey];
+            // first time opening, set default values
+            chrome.storage.sync.set({ [storageKey]: defaultValue }, () => {
+                console.log(`First time setup: ${storageKey} saved as ${defaultValue}`);
+            });
         }
     });
 
+    // send message to contentScript on value change
     selectMenu.addEventListener('change', (event) => {
         const value = selectMenu.type === 'checkbox' ? selectMenu.checked : event.target.value;
 
@@ -51,6 +64,7 @@ function dropDownMenuActions(elementId, storageKey, msgCmd) {
             );
         });
 
+        // update stored value
         chrome.storage.sync.set({ [storageKey]: value }, () => {
             console.log(`${storageKey} saved as ${value}`);
         });
@@ -60,6 +74,6 @@ function dropDownMenuActions(elementId, storageKey, msgCmd) {
 function documentEvents() {
     // Setup dropdown menus
     DROPDOWN_MENU_PARAMS.forEach((it) => {
-        dropDownMenuActions(it['elementId'], it['storageKey'], it['msgCmd']);
+        dropDownMenuActions(it['elementId'], it['storageKey'], it['msgCmd'], it['defaultValue']);
     });
 }
